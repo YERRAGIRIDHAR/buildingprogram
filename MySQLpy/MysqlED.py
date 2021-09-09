025e41e8e6969ffc16b64228da017e62a15503f4
giridharyerra <giridharyerra07@gmail.com>
2021-09-09 17:50:50 +0530

    init

--- MySQLpy/MysqlED.py
+++ MySQLpy/MysqlED.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from difflib import get_close_matches 
 
 con = mysql.connector.connect(
 user = "ardit700_student",
@@ -10,7 +9,6 @@ database = "ardit700_pm1database"
 
 cursor = con.cursor()
 
-
 word=input("Enter the word: ")
 
 query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
@@ -18,6 +16,6 @@ results = cursor.fetchall()
 
 if results:
     for result in results:
-        print(results[0])
+        print(result[0])
 else:
-    print("No word found")
+    print("No word found!")
