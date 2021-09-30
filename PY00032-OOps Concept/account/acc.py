f12700b26bf6a229e83bdba90ec6fa6f0389fe97
YERRAGIRIDHAR <giridharyerra07@gmail.com>
2021-09-30 15:52:26 +0530

    OOPs Concept

--- PY00032-OOps Concept/account/acc.py
+++ PY00032-OOps Concept/account/acc.py
@@ -33,23 +33,25 @@ class Checking(Account):
 
     """This class Generates checking account objects"""
 
-    type = "checking"
+    type = "Checking"
 
     def __init__(self, filepath, fee):
         Account.__init__(self, filepath) #Calling init method of above classs
         self.fee = fee  #data member
 
     def transfer(self, amount): #class method here transfer is a method
-        self.balance = self.balance-amount-self.fee
+        self.balance = self.balance- amount - self.fee
 
-Giri_checking = Checking("account\\Giri.txt", 1) #Object instance
-Giri_checking.transfer(1200)
-print(Giri_checking.balance)
-Giri_checking.commit()
+giri_checking = Checking("account\\giri.txt", 1) #Object instance
+giri_checking.transfer(100)
+print(giri_checking.balance)
+giri_checking.commit()
     
 
-Teja_checking = Checking("account\\Teja.txt", 1) #Object instance
-Teja_checking.transfer(1200)
-print(Teja_checking.balance)
-Teja_checking.commit()
+teja_checking = Checking("account//teja.txt", 1) #Object instance
+teja_checking.transfer(100)
+print(teja_checking.balance)
+teja_checking.commit()
     
+
+print(giri_checking.__doc__)
